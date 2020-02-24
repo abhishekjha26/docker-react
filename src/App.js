0adeb1c994a9cbb580c7b5b00bed464ed7a2ab19
abhishek.jha <abhishekjha26@live.com>
2020-02-24 20:36:53 +0530

    testing auto deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changed to feature branch
+          Changed to feature branch 1
         </p>
         <a
           className="App-link"
